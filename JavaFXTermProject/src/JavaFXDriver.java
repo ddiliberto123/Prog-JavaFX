573fcfe144af9cf392de0067201a3ff0521f056a
Daniel Diliberto <daniel.diliberto@hotmail.com>
2023-09-10 19:01:33 -0400

    Orbit & Plot
    Initial computational classes and algorithms for plot and orbit have been added

--- JavaFXTermProject/src/JavaFXDriver.java
+++ JavaFXTermProject/src/JavaFXDriver.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
  */
 
+import java.util.ArrayList;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -10,6 +11,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.scene.chart.*;
 
 /**
  *
@@ -19,22 +21,33 @@ public class JavaFXDriver extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+        
+        NumberAxis xAxis = new NumberAxis();
+        NumberAxis yAxis = new NumberAxis();
+        xAxis.setLabel("x (AU)");
+        yAxis.setLabel("y (AU)");
+        final ScatterChart<Number,Number> scatterChart = new ScatterChart<>(xAxis,yAxis);
+        scatterChart.setTitle("Orbits");
+        EllipticalOrbit earth = new EllipticalOrbit(0.017,1.00000);
+        ArrayList<EllipticalOrbit> bodiesArray = new ArrayList<>();
+        bodiesArray.add(earth);
+        double rate = .01;
+        XYChart.Series series = new XYChart.Series();
+        for (EllipticalOrbit e : bodiesArray){
+            System.out.println(e.semiMajorAxis + " " + e.semiMinorAxis);
+            System.out.println((1-(Math.pow(5,2)/(Math.pow(e.semiMajorAxis,2)))));
+            System.out.println(Math.sqrt((1-(Math.pow(5,2)/(Math.pow(e.semiMajorAxis,2))))*Math.pow(e.semiMinorAxis,2)));
+            for (double i = -e.semiMajorAxis; i < e.semiMajorAxis; i+=rate){
+                series.getData().add(new XYChart.Data(i,Math.sqrt((1-(Math.pow(i,2)/(Math.pow(e.semiMajorAxis,2))
+                        ))*Math.pow(e.semiMinorAxis,2))));
+                series.getData().add(new XYChart.Data(i,-(Math.sqrt((1-(Math.pow(i,2)/(Math.pow(e.semiMajorAxis,2))
+                        ))*Math.pow(e.semiMinorAxis,2)))));
             }
-        });
+        }
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
+        Scene scene = new Scene(scatterChart,600,500);
+        scatterChart.getData().add(series);
+        primaryStage.setTitle("Planets");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
@@ -43,9 +56,7 @@ public class JavaFXDriver extends Application {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-//        launch(args);
-        Orbit earth = new Orbit(5.96*Math.pow(10, 24),6.36*Math.pow(10, 6)+4.00*Math.pow(10, 5));
-        System.out.println(earth.orbitalVelocity);
+        launch(args);
     }
     
 }
