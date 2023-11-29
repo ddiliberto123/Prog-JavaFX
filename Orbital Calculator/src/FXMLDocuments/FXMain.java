97c05a8128b23a4bb0a111f0dc08bc71d963bf15
R0ketZz <Samalu2700@gmail.com>
2023-11-29 18:06:30 -0500

    Added Home Screen
    Added Home Screen

--- Orbital Calculator/src/FXMLDocuments/FXMain.java
+++ Orbital Calculator/src/FXMLDocuments/FXMain.java
@@ -20,7 +20,7 @@ public class FXMain extends Application {
     
     @Override
     public void start(Stage primaryStage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("MainScreen.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         
---
+++ Orbital Calculator/src/FXMLDocuments/MainMenuController.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package FXMLDocuments;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author Samuel
+ */
+public class MainMenuController implements Initializable {
+
+    @FXML
+    private Button startButton;
+    @FXML
+    private Button exitButton;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void goToMainPage(ActionEvent event) throws IOException {
+        
+        Parent root = FXMLLoader.load(getClass().getResource("MainScreen.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) startButton.getScene().getWindow(); 
+        stage.setScene(scene);
+        
+    }
+
+    @FXML
+    private void closeApp(ActionEvent event) {
+        
+        Platform.exit(); 
+        
+    }
+    
+}
